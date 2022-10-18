3f250d6f48e1c99ca176e9881af22a8f7c1f1d9f
Shashikant <shashikantprajapti17@gmail.com>
2022-10-18 20:29:33 +0530

    new

---
+++ app.js
@@ -0,0 +1,9 @@
+class emp{
+    constructor(empid, name){
+        this.empid = empid,
+        this.name = name
+    }
+}
+
+emp1 = new emp(1, 'shashikant')
+console.log(emp1)
--- main.js
+++ main.js
@@ -24,63 +24,95 @@
 // console.log(person2)
 // console.log(person2.hasOwnProperty('name'))
 
-function Human(ls) {
-  this.lifeSpan = ls;
-}
-Human.prototype.breath = function () {
-  console.log("Breathing...........");
-};
+// function Human(ls) {
+//   this.lifeSpan = ls;
+// }
+// Human.prototype.breath = function () {
+//   console.log("Breathing...........");
+// };
 
-let human1 = new Human(100);
-let human2 = new Human(200);
+// let human1 = new Human(100);
+// let human2 = new Human(200);
 
 // console.log(human1);
 // console.log(human2);
 // human1.breath();
 // human2.breath();
 
+// function Student(fn, ln, age) {
+//   this.firstName = fn;
+//   this.lastName = ln;
+//   this.age = age;
+// }
+// Student.prototype.fullName = function () {
+//   return this.firstName + " " + this.lastName;
+// };
 
+// Student.prototype.__proto__ = Object.create(Human.prototype);
 
+// let student1 = new Student("rahul", "kumar", "18");
+// let student2 = new Student("Shashikant", "Prajapti", "20");
 
+// console.log(student1);
+// console.log(student2);
+// student1.breath();
+// student2.breath();
+// console.log(student1.fullName());
+// console.log(student2.fullName());
 
+class person{
+  constructor(age, name){
+    this.age = age;
+    this.name = name;
+  }
 
-function Student(fn, ln, age) {
-  this.firstName = fn;
-  this.lastName = ln;
-  this.age = age;
+  sayHi(){
+    console.log(this.age, this.name);
+  }
+
+  static new = "Shashikant"
+  static hello(){
+    console.log("Static Method............")
+  }
 }
-Student.prototype.fullName = function () {
-  return this.firstName + " " + this.lastName;
-};
 
-Student.prototype.__proto__ = Object.create(Human.prototype);
+let person1 = new person(48,"Shashikant");
 
-let student1 = new Student("rahul", "kumar", "18");
-let student2 = new Student("Shashikant", "Prajapti", "20");
+person1.sayHi()
+console.log(person.new)
 
-console.log(student1);
-console.log(student2);
-student1.breath();
-student2.breath();
-console.log(student1.fullName());
-console.log(student2.fullName());
+person1.sayHi()
 
+class emp {
+  constructor(fn) {
+    this.name = fn;
+  }
+  msg() {
+    console.log(this.name, "World");
+  }
+}
 
+class manager extends emp {
+  constructor(ln, fn, sup) {
+    super(sup);
+    this.name = this.name;
+    this.lastname = this.name;
+  }
+  shas() {
+    this.msg()
+  }
+}
 
+class admin1 extends manager {
+    constructor(name){
+      super()
+      this.fname = name;
+    }
+    name12(){
+      console.log(this.fname)
+    }
+}
 
+let admin12 = new admin1("prajapti");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+admin12.name12()
